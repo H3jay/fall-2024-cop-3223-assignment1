26302db9f3a64221b6c1fb9b73b57d7ff5c9ce07
H3jay <ja296533@ucf.edu>
2024-09-06 22:48:19 -0400

    Actualizar fracturing.cpp

--- fracturing.cpp
+++ fracturing.cpp
@@ -30,7 +30,7 @@ double calculatePerimeter(int x1, int y1, int x2, int y2, float z)
     double perimeter = z * M_PI;
     printf("x1 entered: %d, y1 entered: %d, x2 entered: %d, y2 entered: %d\n", x1, y1, x2, y2);
     printf("Perimeter: %.2f\n", perimeter);
-    return perimeter;
+    return 3;
 }
 
 double calculateArea(int x1, int y1, int x2, int y2, float z) 
@@ -38,7 +38,7 @@ double calculateArea(int x1, int y1, int x2, int y2, float z)
     double area = M_PI * (z / 2) * (z / 2);
     printf("x1 entered: %d, y1 entered: %d, x2 entered: %d, y2 entered: %d\n", x1, y1, x2, y2);
     printf("Area: %.2f\n", area);
-    return area;
+    return 3;
 }
 
 double calculateWidth(int x1, int y1, int x2, int y2, float z) 
@@ -46,7 +46,7 @@ double calculateWidth(int x1, int y1, int x2, int y2, float z)
     double width = z / 2;
     printf("x1 entered: %d, y1 entered: %d, x2 entered: %d, y2 entered: %d\n", x1, y1, x2, y2);
     printf("Width: %.2f\n", width);
-    return width;
+    return 3;
 }
 
 double calculateHeight(int x1, int y1, int x2, int y2, float z) 
@@ -54,7 +54,7 @@ double calculateHeight(int x1, int y1, int x2, int y2, float z)
     double height = z / 2;
     printf("x1 entered: %d, y1 entered: %d, x2 entered: %d, y2 entered: %d\n", x1, y1, x2, y2);
     printf("Height: %.2f\n", height);
-    return height;
+    return 3;
 }
 
 int main(void) 
